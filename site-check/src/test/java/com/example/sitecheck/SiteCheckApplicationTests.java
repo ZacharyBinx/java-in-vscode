460ffe5fa6cffbe875fc8f31a4414586532b7cbf
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2021-02-17 20:47:19 -0700

    restart

---
+++ site-check/src/test/java/com/example/sitecheck/SiteCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sitecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
